1168608d89713ad2fa2e5411d8c5ba711ec56196
Eugene Morozenko <morozenko-2014@mial.ru>
2021-09-22 21:43:31 +0300

    Refactoring
    Update component

--- List/src/ToDo/AddToDo/AddToDo.js
+++ List/src/ToDo/AddToDo/AddToDo.js
@@ -1,29 +1,42 @@
 import React, { useState } from "react";
 import "./AddToDo.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function AddToDo({onCreate}) {
-  const [value, setValue] = useState('');
+function useInputValue(defaultValue = "") {
+  const [value, setValue] = useState(defaultValue);
+
+  return {
+      bind: {
+        value,
+        onChange: event => setValue(event.target.value)
+      },
+      clear: () => setValue(''),
+      value: () => value
+  }
+}
+
+function AddToDo({ onCreate }) {
+    const input = useInputValue('')
 
   function submitHandler(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(value.trim()) {
-        onCreate(value)
-        setValue('')
+    if (input.value().trim()) {
+      onCreate(input.value())
+      input.clear()
     }
   }
 
   return (
     <form onSubmit={submitHandler}>
-      <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <input {...input.bind} />
       <button type="submit">Add to do</button>
     </form>
   );
 }
 
 AddToDo.propTypes = {
-    onCreate: PropTypes.func.isRequired
-}
+  onCreate: PropTypes.func.isRequired,
+};
 
 export default AddToDo;
