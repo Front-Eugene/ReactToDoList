1aa13aa037d002c3ae52b340b623e7612658a2d9
Eugene Morozenko <morozenko-2014@mial.ru>
2021-09-22 12:25:44 +0300

    Beautiful wrapper
    A little css for items

--- List/src/ToDo/ToDoItem/ToDoItem.js
+++ List/src/ToDo/ToDoItem/ToDoItem.js
@@ -1,12 +1,23 @@
 import React from "react";
+import './ToDoItems.css'
+import PropTypes from "prop-types";
 
 function ToDoItem({ todo, index }) {
   return (
     <li>
-      <strong>{index + 1}</strong>
-      {todo.title}
+      <span>
+        <input type="checkbox" />
+        <strong>{index + 1}</strong>
+        {todo.title}
+      </span>
+        <button>&times;</button>
     </li>
   );
 }
+
+ToDoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+};
 
 export default ToDoItem;
--- List/src/ToDo/ToDoList.js
+++ List/src/ToDo/ToDoList.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ToDoItem from './ToDoItem/ToDoItem';
-import './ToDoList.css';
+import React from "react";
+import ToDoItem from "./ToDoItem/ToDoItem";
+import PropTypes from "prop-types";
+import "./ToDoList.css";
 
 function ToDoList(props) {
   return (
     <ul>
-        {props.todos.map((todo, index) => {
-            return <ToDoItem todo={todo} key={todo.id} index={index} />
-        })}
+      {props.todos.map((todo, index) => {
+        return <ToDoItem todo={todo} key={todo.id} index={index} />;
+      })}
     </ul>
   );
 }
+
+ToDoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 
 export default ToDoList;
